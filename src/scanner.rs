20a2cad7c1036f016711de1c18421ad52f5c8cf2
Ben Scholar <bscholar@Bens-MacBook-Pro.local>
2019-09-07 14:07:21 -0400

    moved keywords map to token file, makes more sense

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,32 +3,8 @@ use crate::token::*;
 use crate::error_handling;
 use crate::util;
 
-use std::collections::HashMap;
 use std::vec::Vec;
 use std::iter::FromIterator;
-
-lazy_static! {
-    static ref KEYWORDS: HashMap<&'static str, TokenType> = {
-        let mut m = HashMap::new();
-        m.insert("and",    TokenType::And);
-        m.insert("class",  TokenType::Class);
-        m.insert("else",   TokenType::Else);
-        m.insert("false",  TokenType::False);
-        m.insert("for",    TokenType::For);
-        m.insert("fun",    TokenType::Fun);
-        m.insert("if",     TokenType::If);
-        m.insert("nil",    TokenType::Nil);
-        m.insert("or",     TokenType::Or);
-        m.insert("print",  TokenType::Print);
-        m.insert("return", TokenType::Return);
-        m.insert("super",  TokenType::Super);
-        m.insert("this",   TokenType::This);
-        m.insert("true",   TokenType::True);
-        m.insert("var",    TokenType::Var);
-        m.insert("while",  TokenType::While);
-        m
-    };
-}
 
 #[derive(Debug)]
 pub struct Scanner {
--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+
+lazy_static! {
+    pub static ref KEYWORDS: HashMap<&'static str, TokenType> = {
+        let mut m = HashMap::new();
+        m.insert("and",    TokenType::And);
+        m.insert("class",  TokenType::Class);
+        m.insert("else",   TokenType::Else);
+        m.insert("false",  TokenType::False);
+        m.insert("for",    TokenType::For);
+        m.insert("fun",    TokenType::Fun);
+        m.insert("if",     TokenType::If);
+        m.insert("nil",    TokenType::Nil);
+        m.insert("or",     TokenType::Or);
+        m.insert("print",  TokenType::Print);
+        m.insert("return", TokenType::Return);
+        m.insert("super",  TokenType::Super);
+        m.insert("this",   TokenType::This);
+        m.insert("true",   TokenType::True);
+        m.insert("var",    TokenType::Var);
+        m.insert("while",  TokenType::While);
+        m
+    };
+}
 
 #[allow(non_snake_case, dead_code)]
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
